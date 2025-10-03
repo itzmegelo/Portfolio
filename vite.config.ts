c9383338a9fdbc4f6f3498995f0ee4f9301849e1
itzmegelo <maldora.pojas.angelo@gmail.com>
2025-10-03 17:19:42 +0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
+  base: '/variance-high.vercel.app/',
   plugins: [vue(),tailwindcss(),vueDevTools()],
 })
